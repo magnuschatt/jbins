e8fa7f6b330bfc5b21786477a3503d140aaf70b0
Magnus Vinther <mvi@systematic.com>
2017-11-15 17:05:51 +0100

    Connected to the database!

--- core/src/main/kotlin/chatt/jbins/Main.kt
+++ core/src/main/kotlin/chatt/jbins/Main.kt
@@ -1,5 +1,50 @@
 package chatt.jbins
 
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.SQLException
+
+
 fun main(args: Array<String>) {
-    println("Hello world")
+    println("-------- PostgreSQL " + "JDBC Connection Testing ------------")
+
+    try {
+
+        Class.forName("org.postgresql.Driver")
+
+    } catch (e: ClassNotFoundException) {
+
+        println("Where is your PostgreSQL JDBC Driver? " + "Include in your library path!")
+        e.printStackTrace()
+        return
+
+    }
+
+
+    println("PostgreSQL JDBC Driver Registered!")
+
+    val connection: Connection?
+
+    try {
+
+        connection = DriverManager.getConnection(
+                "jdbc:postgresql://docker:5432/jbins",
+                "postgres",
+                "postgres"
+        )
+
+    } catch (e: SQLException) {
+
+        println("Connection Failed! Check output console")
+        e.printStackTrace()
+        return
+
+    }
+
+
+    if (connection != null) {
+        println("You made it, take control your database now!")
+    } else {
+        println("Failed to make connection!")
+    }
 }
